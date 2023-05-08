a471488ca3c0b9d5ebdd3eb1dbafdd6e7c6e4dbd
Sunilthecoder007 <sunil.cbcwebs@gmail.com>
2023-05-08 12:42:17 +0530

    initial commit

---
+++ client/src/app/services/ApiPath.js
@@ -0,0 +1,17 @@
+export const apiPath = {
+  // Authentication
+  signUp: "auth/register",
+  login: "auth/login",
+  logout: "auth/logout",
+  forgotPassword: "forgot-password",
+  resetPassword: "auth/reset-password",
+  //User
+  user: "users/",
+  //Articles
+  postArticle: "article",
+  categoryArticle: "article/category",
+  searchArticles: "article/search",
+};
+export const API_URL = "https://blogzillaserver-production.up.railway.app/v1/";
+export const SERVER_URL = "https://blogzillaserver-production.up.railway.app/";
+// export const API_URL = process.env.REACT_APP_API_URL;
